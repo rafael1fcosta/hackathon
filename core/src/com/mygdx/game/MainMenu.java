d8799d516ccf9b555f0ff2f0654048d5ac125f64
miguelmendes <m.mendes1996@gmail.com>
2019-08-02 00:30:07 +0100

    ...

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -1,11 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
-import com.mygdx.game.game1.GameOne;
 
 public class MainMenu extends ScreenAdapter {
 
@@ -14,6 +11,11 @@ public class MainMenu extends ScreenAdapter {
     private Texture menuBackground = new Texture(Gdx.files.internal("mainmenubackground.jpg"));
     private Texture playButton = new Texture(Gdx.files.internal("playButton.png"));
     private Texture exitButton = new Texture(Gdx.files.internal("exitButton.jpg"));
+    private CharSequence str = "Hello World!";
+    private CharSequence str1 = "Hgrthjfdello World!";
+    private CharSequence str2 = "Hello Wodgfhntumtuykjhgfhjkhrld!";
+
+
 
 
     public MainMenu(ScreenManager screenManager) {
@@ -24,17 +26,15 @@ public class MainMenu extends ScreenAdapter {
     public void changeScreen() {
         if (Gdx.input.isTouched()) {
             if (Gdx.input.getX() > 104.8f && Gdx.input.getX() < 304.8f && Gdx.input.getY() > 334 && Gdx.input.getY() < 434) {
-                System.out.println("aconteceu. parabens.");
                 screenManager.setScreen(new OptionsMenu(screenManager));
-
             }
         }
     }
-
         @Override
         public void render ( float delta){
 
             screenManager.batch.begin();
+
             screenManager.batch.draw(menuBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
             screenManager.batch.draw(playButton, Gdx.graphics.getWidth() / 5 - 200 / 2, Gdx.graphics.getHeight() / 2 - 100 / 2);
             screenManager.batch.draw(exitButton, Gdx.graphics.getWidth() / 2 - 200 / 2, Gdx.graphics.getHeight() / 2 - 100 / 2);
