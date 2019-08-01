af122f410a564704e2035dc966ad8271591a77de
docKoder <1802898@estudante.uab.pt>
2019-08-02 00:54:19 +0100

    outro

--- core/src/com/mygdx/game/TextureManager.java
+++ core/src/com/mygdx/game/TextureManager.java
@@ -11,16 +11,12 @@ public abstract class TextureManager {
 
     }
 
-
-
-
-
     public static class Game2 {
 
         public static String prefix ="game2/";
         public static  final Texture PLAYER = new Texture(Gdx.files.internal(prefix + "badlogic.jpg"));
         public static final Texture MISSILE = new Texture(Gdx.files.internal(prefix +"missile.png"));
-        public static final Texture ENEMY = new Texture(Gdx.files.internal(prefix +"badlogic.jpg"));
+        public static final Texture ENEMY = new Texture(Gdx.files.internal(prefix +"bandit.png"));
         public static final Texture BACKGROUND = new Texture(Gdx.files.internal(prefix +"background.png"));
 
     }
