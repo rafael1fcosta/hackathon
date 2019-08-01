300964f1edd0023efcdf45294ca78afaa5cc8a75
miguelmendes <m.mendes1996@gmail.com>
2019-08-01 18:57:35 +0100

    added hashmap for screens

--- core/src/com/mygdx/game/ScreenManager.java
+++ core/src/com/mygdx/game/ScreenManager.java
@@ -1,24 +1,40 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.HashMap;
 
 public class ScreenManager extends Game {
 
     SpriteBatch batch;
     ShapeRenderer shapeRenderer;
     BitmapFont font;
+    HashMap<ScreenType, ScreenAdapter> screenMap;
+
+    MainMenu mainMenu;
+
 
     @Override
     public void create () {
 
 
+
+        screenMap=new  HashMap<>();
+
+        screenMap.put(ScreenType.MAINMENU, new MainMenu(this));
+
+
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
         font = new BitmapFont();
-        setScreen(new MainMenu(this));
+        setScreen(screenMap.get(ScreenType.MAINMENU));
+
+
     }
 
     @Override
