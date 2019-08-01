e82933bf946c2c2fea1156852a28e024c15891b3
miguelmendes <m.mendes1996@gmail.com>
2019-08-01 18:24:19 +0100

    added screen manager and configured screen size

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,10 +3,15 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.CrookedGame;
+import com.mygdx.game.MainMenu;
+import com.mygdx.game.ScreenManager;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CrookedGame(), config);
+		new LwjglApplication(new ScreenManager(), config);
+
+		config.width = 1024;
+		config.height= 768;
 	}
 }
