39829835bc1a20159a00ce1e5ab9d45728a17817
rafaelcosta <rafael1fcosta@gmail.com>
2019-08-01 18:28:21 +0100

    to add screen size

--- core/src/com/mygdx/game/CrookedGame.java
+++ core/src/com/mygdx/game/CrookedGame.java
@@ -5,14 +5,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.game1.Player;
 
 public class CrookedGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Player player;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
+
 		img = new Texture("badlogic.jpg");
 	}
 
---
+++ core/src/com/mygdx/game/game1/Player.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.game1;
+
+public class Player {
+}
